b16641441388bbd85729d0e5af7358083ab1ef1e
Luis Alejandro Bordo García <bgluiszz@gmail.com>
2023-08-18 20:14:38 +0200

    Test of the LLM using Luis' MA thesis #18
    - Also updated .gitignore:
    to stop ignoring test documents at
    tests/mocks/source_documents

---
+++ tests/gateways/test_localgpt_gateway_LABG.py
@@ -0,0 +1,25 @@
+from pathlib import Path
+from lib.infrastructure.config.containers import Container
+
+
+def test_localgpt_embedding_service(app_container: Container):
+    localgpt_embedding_service = app_container.localgpt_embedding_gateway()
+    assert localgpt_embedding_service is not None
+    thesis_pdf = Path("tests/mocks/source_docs/MA_Thesis_LABG.pdf")
+    assert thesis_pdf.exists()
+    assert thesis_pdf.is_file()
+    document = localgpt_embedding_service.load_single_document(path=str(thesis_pdf))
+    localgpt_embedding_service.create_embeddings(documents=[document])
+
+    localgpt_inference_service = app_container.localgpt_inference_gateway()
+    assert localgpt_inference_service is not None
+
+    qa = localgpt_inference_service.qa
+
+    query = "What were Pareto's main contributions to the development of Expected Utility Theory?"
+
+    res = qa(query)
+    answer, documents = res["result"], res["source_documents"]
+    print(f"Query: {query}\nAnswer: {answer}\nDocuments: {documents}")
+    assert answer is not None
+    assert len(documents) > 0
